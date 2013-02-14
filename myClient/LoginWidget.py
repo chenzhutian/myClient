fca01ad552184ce70f5a5abc255d0a3b193158fe
Chenzhutian <chenzhutian1992@gmail.com>
2013-02-14 15:48:47 +0800

    登陆模块完成
    登陆模块完成，登陆不成有提示，登陆后获得乱码、cookie、还有姓名以及别的链接等

--- myClient/LoginWidget.py
+++ myClient/LoginWidget.py
@@ -9,20 +9,120 @@ import urllib.request
 import urllib.parse
 from html.parser import HTMLParser
 
-class loginWidget(object):
-    class LoginParser(HTMLParser):
-        pass
+class MenuPath:
     
+    xsxk = ''
+    xtyk = ''
+    cxhbxxk = ''
+    bkbmqr = ''
+    fxbm = ''
+    fxxk = ''
+    yxxxk = ''
+    grxx = ''
+    mmxg = ''
+    zytjkbcx = ''
+    xsgrkb = ''
+    cjcx = ''
+    pyjh = ''
+    jscx = ''
+    kcjscx = ''
+    xsxkqkcx = ''
+    myddc = ''
+
+
+class LoginParser(HTMLParser):
+        
+    tagflag = {'script':False,
+               'span':False}
+    dataflag = {'script language="javascript"':False,
+                'span id="xhxm"':False}
+    loginError = ''
+    xm = ''
+    path = MenuPath()
+    def encodeUrl(self,s):
+        pos = s.find(self.xm)
+        targetUrl = s[:pos]+urllib.parse.quote(s[pos:pos+len(self.xm)].encode('gb2312'))+s[pos+len(self.xm):]
+        return targetUrl
+    
+    def handle_starttag(self, tag, attrs):
+        if tag == 'script':
+            for name,value in attrs:
+                if name == 'language':
+                    if value == 'javascript':
+                        self.tagflag['script'] = True
+                        self.dataflag['script language="javascript"'] = True
+        elif tag == 'span':
+            for name,value in attrs:
+                if name == 'id':
+                    if value == 'xhxm':
+                        self.tagflag['span'] = True
+                        self.dataflag['span id="xhxm"']=True
+        elif tag == 'a':
+            for name,value in attrs:
+                if name == 'onclick':
+                    if value == "GetMc('学生选课');":
+                        self.path.xsxk = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('选体育课');":
+                        self.path.xtyk = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('重修或补修选课');":
+                        self.path.cxhbxxk = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('补考报名确认');":
+                        self.path.bkbmqr = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('辅修报名');":
+                        self.path.fxbm = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('院系选修课');":
+                        self.path.yxxxk = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('个人信息');":
+                        self.path.grxx = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('密码修改');":
+                        self.path.mmxg = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('专业推荐课表查询');":
+                        self.path.zytjkbcx = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('学生个人课表');":
+                        self.path.xsgrkb = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('成绩查询');":
+                        self.path.cjcx = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('培养计划');":
+                        self.path.pyjh = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('教室查询');":
+                        self.path.jscx = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('课程介绍查询');":
+                        self.path.kcjscx = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('学生选课情况查询');":
+                        self.path.xsxkqkcx = self.encodeUrl(attrs[0][1])
+                    elif value == "GetMc('满意度调查');":
+                        self.path.myddc = self.encodeUrl(attrs[0][1])
+
+    def handle_data(self,data):
+        if self.tagflag['script'] == True:
+            if self.dataflag['script language="javascript"']:
+                if data[:5] == 'alert':
+                    self.loginError = data[7:]
+                    self.loginError = self.loginError[:self.loginError.find("'")]
+        elif self.tagflag['span']:
+            if self.dataflag['span id="xhxm"']:
+                self.xm = data[14:-2]
+        
+    def handle_endtag(self,tag):
+        if tag == 'script':
+            self.tagflag['script'] = False
+        elif tag == 'span':
+            self.tagflag['span'] = False
+
+
+class loginWidget(object):
+
     userName = ''
     userCode = ''
     userCookie = ''
     checkCode = ''
-    mianUrl = ''
-    xm = ''
+    mainUrl = ''
+    userXm = ''
     checkCodePath = 'CheckCode.aspx'
     loginPath = 'default2.aspx'
-    loginParser = LoginParser()
+    mainPath = ''
     loginState = False
+    menuPath = MenuPath()
     
     def __init__(self):
         f = urllib.request.urlopen('http://jw2005.scuteo.com/')
@@ -32,6 +132,7 @@ class loginWidget(object):
         self.userCookie = Cookie[:Cookie.find(';')]
         self.root = tk.Tk()
         self.creatWidget()
+        self.loginParser = LoginParser()
         
     def creatWidget(self):
         localjpg = ""+('checkCode.gif')
@@ -54,10 +155,10 @@ class loginWidget(object):
         self.checkCodelabel.grid(row = 2,column = 0,sticky = tk.W)
         self.checkCodeEntry = tk.Entry(self.loginFrame,width = 8)
         self.checkCodeEntry.grid(row = 2,column = 1,ipadx = 10,sticky = tk.E)
-        self.image = tk.PhotoImage(file = './checkCode.gif')
-        self.C = tk.Canvas(self.loginFrame,height = 20,width = 60)
-        self.C.create_image(35,10,image = self.image)
-        self.C.grid(row = 2,column = 2,sticky = tk.E)
+        self.checkCodeImage = tk.PhotoImage(file = './checkCode.gif')
+        self.checkCodeCanvas = tk.Canvas(self.loginFrame,height = 20,width = 60)
+        self.checkCodeCanvas.create_image(35,10,image = self.checkCodeImage)
+        self.checkCodeCanvas.grid(row = 2,column = 2,sticky = tk.E)
 
         self.loginButton = tk.Button(self.loginFrame,text = '登陆',width = 7,command = self.login)
         self.loginButton.grid(row = 3,column = 1)
@@ -65,10 +166,49 @@ class loginWidget(object):
         self.quitButton.grid(row = 3,column = 2)
         
     def login(self):
-        if len(self.userNameEntry.get()) != 12:
+        self.userName = self.userNameEntry.get()
+        self.userCode = self.userCodeEntry.get()
+        self.checkCode = self.checkCodeEntry.get()
+        
+        if len(self.userName) != 12:
             tkinter.messagebox.showwarning('错误', '用户名长度不对')
-        elif len(self.checkCodeEntry.get()) != 5:
+        elif len(self.userCode) == 0:
+            tkinter.messagebox.showwarning('错误', '密码不能为空')
+        elif len(self.checkCode) != 5:
             tkinter.messagebox.showwarning('错误', '验证码不正确')
+        else:
+            self.mainPath = 'xs_main.aspx?xh='+self.userName
+            
+            bodypart1 = '__VIEWSTATE=dDwtMTg3MTM5OTI5MTs7PkfLdDpkwXkZwjVjoRLwfK%2BL%2FuEU&TextBox1='
+            bodypart2 = '&TextBox2='
+            bodypart3 = '&TextBox3='
+            bodypart4 = '&RadioButtonList1=%D1%A7%C9%FA&Button1=&lbLanguage='
+            self.body = bodypart1+self.userName+bodypart2+self.userCode+bodypart3+self.checkCode+bodypart4
+            self.body = self.body.encode('ISO-8859-1')
+            
+            self.headers = {'Host':'jw2005.scuteo.com',
+                           'Connection':'keep-alive',
+                           'Content-Length':len(self.body),
+                           'Cache-Control':'max-age=0',
+                           'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                           'Origin':'http://jw2005.scuteo.com',
+                           'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17',
+                           'Content-Type':'application/x-www-form-urlencoded',
+                           'Referer':self.mainUrl+self.loginPath,
+                           'Accept-Encoding':'gzip,deflate,sdch',
+                           'Accept-Language':'zh-CN,zh;q=0.8',
+                           'Accept-Chaset':'GBK,utf-8;q=0.7,*;q=0.3',
+                           'Cookie':self.userCookie}
+            
+            self.req = urllib.request.Request(url = self.mainUrl+self.loginPath,data = self.body,headers = self.headers)
+            self.data = urllib.request.urlopen(url = self.req)
+            loginParser = LoginParser()
+            loginParser.feed(self.data.read().decode('gb2312'))
+            if loginParser.loginError != '' :
+                tkinter.messagebox.showwarning('错误', loginParser.loginError)
+            else:
+                self.userXm = loginParser.xm
+                self.menuPath = loginParser.path
 
 def main():
     loginWidget()
